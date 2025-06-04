e0b5e4667cacece24258684e45e0acf57d2f5ae7
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-06-04 19:36:52 +0000

    speed test

---
+++ speed/src/components/ SpeedTypingGame.js
@@ -0,0 +1,39 @@
+const paragraphs = [
+    `A plant is one of the most important living things that 
+    develop on the earth and is made up of stems, leaves, 
+    roots, and so on. Parts of Plants: The part of the plant
+    that developed beneath the soil is referred to as root 
+    and the part that grows outside of the soil is known as shoot.
+    The shoot consists of stems, branches, leaves, fruits, 
+    and flowers. Plants are made up of six main parts: roots, stems,
+    leaves, flowers, fruits, and seeds.`,
+
+    `The root is the part of the plant that grows in the soil. 
+    The primary root emerges from the embryo. Its primary
+    function is to provide the plant stability in the earth
+    and make other mineral salts from the earth available to the plant
+    for various metabolic processes. There are three types of roots i.e. Tap Root, 
+    Adventitious Roots, and Lateral Root.`,
+
+    `Stem is the posterior part that remains above the ground 
+    and grows negatively geotropic. Internodes and nodes are 
+    found on the stem. Branch, bud, leaf, petiole, flower, and
+    inflorescence on a node are all those parts of the plant 
+    that remain above the ground and undergo negative subsoil 
+    development.`,
+
+    `It is the blossom of a plant. A flower is the part of a plant 
+    that produces seeds, which eventually become other flowers. They 
+    are the reproductive system of a plant. Most flowers consist of 
+    4 main parts: sepals, petals, stamens, and carpels.
+    The female portion of the flower is the carpel. The majority 
+    of flowers are hermaphrodites,
+    meaning they have both male and female components.`,
+
+    `An aunt is a bassoon from the right perspective. 
+    As far as we can estimate, some posit the melic Myanmar to 
+    be less than kutcha. One cannot separate foods from blowzy bows.
+    The scampish closet reveals itself as a sclerous llama to 
+    those who look. A hip is the skirt of a peak. Some hempy laundries 
+    are thought of simply as orchids.`
+];
